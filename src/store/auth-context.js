44417d7c624b7676f12ca960f3e96ee79a4ec2d0
mhans003 <michaeledwardhanson@gmail.com>
2022-05-02 04:14:35 -0700

    Added reference to onLogout in AuthContext object

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 //Create context object which contains login state
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    //Add 'dummy' function to onLogout just for autocomplete reference when referencing the AuthContext object.
+    onLogout: () => {}
 });
 
 export default AuthContext;
